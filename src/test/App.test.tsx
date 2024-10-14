771071712c4b707e9daa96eb38ccf60ea88b97dd
Samoy <samoy_young@163.com>
2024-10-14 19:01:08 +0800

    feat: 添加 Tauri桌面应用的基本 UI 和功能- 更新 App.tsx，实现基本的 Tauri 桌面应用界面，包括欢迎信息和 greet 功能 - 添加 App.test.tsx 中的测试用例，模拟和验证 greet 功能 - 更新 .gitignore，添加测试快照目录忽略项 - 调整 package.json、tsconfig.json 和 vite.config.ts 配置，以适应新功能

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -1,14 +1,33 @@
-// src/__tests__/App.test.tsx
-
+import { mockIPC } from '@tauri-apps/api/mocks'
+import { userEvent } from '@vitest/browser/context'
 import { expect, test } from 'vitest'
 import { render } from 'vitest-browser-react'
 import App from '../App'
+const setup = () => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  mockIPC(async (cmd, payload): Promise<any> => {
+    switch (cmd) {
+      case 'greet':
+        return `Hello, ${(payload as Record<string, string>)?.name}! You've been greeted from Rust!`
+      default:
+        throw new Error(`Unknown command:${cmd}`)
+    }
+  })
+  const utils = render(<App />)
+  const input = utils.getByPlaceholder('Enter name...')
+  const button = utils.getByRole('button')
+  return {
+    input,
+    button,
+    ...utils,
+  }
+}
 
-test('renders name', async () => {
-  const { getByText, getByRole } = render(<App name="Vitest" />)
-
-  await expect.element(getByText('Hello Vitest x1!')).toBeInTheDocument()
-  await getByRole('button', { name: 'Increment ' }).click()
-
-  await expect.element(getByText('Hello Vitest x2!')).toBeInTheDocument()
+test('Calls the greet function with the provided name', async () => {
+  const { input, button } = setup()
+  const name = 'Samoy'
+  await userEvent.fill(input, name)
+  await userEvent.click(button)
+  const p = document.querySelectorAll('p')
+  expect(p[1]).toHaveTextContent(`Hello, ${name}! You've been greeted from Rust!`)
 })
